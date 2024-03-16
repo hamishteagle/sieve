15ad5c8d422c7a2c20f0e018f6ebc7eab3d6d98d
hamishteagle <64635817+hamishteagle@users.noreply.github.com>
2024-03-16 11:55:12 +0000

    Move method into another file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod sieve;
 use std::io;
 
 fn main() {
@@ -9,28 +10,6 @@ fn main() {
         .expect("Please provide a valid number to calculate primes up to");
 
     let input_number: i64 = input_number.trim().parse().expect("Must be a number!");
-    let primes = sieve(&input_number);
+    let primes = sieve::sieve(&input_number);
     println!("Found primes {:#?}", primes)
-}
-
-fn sieve(&up_to: &i64) -> Vec<i64> {
-    //Starting lowest prime = 2
-    let mut prime_index: usize = 0;
-    // Put all values up to the target value in the initial vector
-    let mut primes = Vec::from_iter(2..up_to);
-
-    while prime_index < primes.len() {
-        let mut composite = primes[prime_index];
-        while composite < up_to {
-            composite += primes[prime_index];
-            if primes.contains(&composite) {
-                let index = primes.iter().position(|x| *x == composite).unwrap();
-                primes.remove(index);
-            }
-        }
-        prime_index += 1;
-    }
-    {
-        primes
-    }
 }
---
+++ src/sieve.rs
@@ -0,0 +1,21 @@
+pub fn sieve(&up_to: &i64) -> Vec<i64> {
+    //Starting lowest prime = 2
+    let mut prime_index: usize = 0;
+    // Put all values up to the target value in the initial vector
+    let mut primes = Vec::from_iter(2..up_to);
+
+    while prime_index < primes.len() {
+        let mut composite = primes[prime_index];
+        while composite < up_to {
+            composite += primes[prime_index];
+            if primes.contains(&composite) {
+                let index = primes.iter().position(|x| *x == composite).unwrap();
+                primes.remove(index);
+            }
+        }
+        prime_index += 1;
+    }
+    {
+        primes
+    }
+}
